bcd26c96eba680e06bdefc6d401f406ae2e70df4
中嶋晋作 <nakashimadays365@nakashimaDays365.local>
2022-10-12 14:56:12 +0900

    first commit

---
+++ init.lua
@@ -0,0 +1,15 @@
+require('settings.base')
+require('settings.highlights')
+require('settings.maps')
+require('settings.plugins')
+
+local has = vim.fn.has
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('settings.macos')
+end
+if is_win then
+  require('settings.windows')
+end
---
+++ plugin/lspsaga.rc.lua
@@ -0,0 +1,17 @@
+local status, saga = pcall(require, "lspsaga")
+if (not status) then return end
+
+saga.init_lsp_saga {
+  server_filetype_map = {
+    typescript = 'typescript',
+    gopls = 'gopls'
+  }
+}
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', 'gl', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+vim.keymap.set('n', 'go', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
+vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
